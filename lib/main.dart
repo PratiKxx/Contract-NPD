083e28230acdc221b71d0c258b42f3f5f0c2ea62
NoelJomichan <noel.jomichan@gmail.com>
2023-12-04 18:03:51 +0530

    Initialize error

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
 import 'addnote.dart';
 import 'editnote.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
